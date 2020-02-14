e23154b9ae7ae0322a27df7bf13dc7e350d8e4e3
bheebsh <benjamin.heebsh@gmail.com>
2020-02-14 08:20:34 -0500

    Adding emacs configuration

---
+++ .emacs.d/elpa/dracula-theme-20200209.1352/dracula-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dracula-theme" "20200209.1352" "Dracula Theme" '((emacs "24")) :commit "05937df96f361da7000a9ae8a57f49fc52df7c89" :authors '(("film42")) :maintainer '("film42") :url "https://github.com/dracula/emacs")
