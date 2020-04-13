7c27bd2164e222796ffec78c358f1237742b7f56
Benjamin Heebsh <benjamin.heebsh@gmail.com>
2020-04-13 13:12:30 -0400

    Changed: Setup files now for WSL

--- .restore/setup.sh
+++ .restore/setup.sh
@@ -3,14 +3,9 @@
 # Setup script to restore installation
 
 # install packages
-yay -Syu --needed - < ~/.restore/arch-packages.txt
-
-# Next setup directories
-rm -r Documents Music Pictures 
-
-ln -s Dropbox/Documents Documents
-ln -s Dropbox/Pictures Pictures
-ln -s Dropbox/Music  Music 
+sudo apt update
+sudo apt upgrade
+xargs -a ~/.restore/packages sudo apt install -y 
 
 # Change default shell to fish
 chsh -s /bin/fish
@@ -19,3 +14,13 @@ chsh -s /bin/fish
 git config --global status.showUntrackedFiles no
 git config --global user.name "Benjamin Heebsh"
 git config --global user.email "benjamin.heebsh@gmail.com"
+
+# setup st3
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install sublime-text
+
+git clone https://github.com/bheebsh/sublime-text-config.git ~/.config/sublime-text-3/Packages/User
+
