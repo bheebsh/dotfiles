b68229eb228eafb4f23d72e2cfd56c33fa1928bd
Benjamin Heebsh <benjamin.heebsh@gmail.com>
2020-04-14 15:46:33 -0400

    Added: Python link in setup script

--- .restore/setup.sh
+++ .restore/setup.sh
@@ -24,3 +24,5 @@ sudo apt install sublime-text
 
 git clone https://github.com/bheebsh/sublime-text-config.git ~/.config/sublime-text-3/Packages/User
 
+# Set python correctly
+ln -s /usr/bin/python /usr/bin/python3.8
