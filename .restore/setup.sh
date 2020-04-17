7cd88f8eb4eb8ff6accd3f91f72fd1eb88250e6e
Benjamin Heebsh <benjamin.heebsh@gmail.com>
2020-04-17 17:43:49 -0400

    Updated: New machine setup

--- .restore/setup.sh
+++ .restore/setup.sh
@@ -15,17 +15,12 @@ git config --global status.showUntrackedFiles no
 git config --global user.name "Benjamin Heebsh"
 git config --global user.email "benjamin.heebsh@gmail.com"
 
-# setup st3
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
-sudo apt install sublime-text
-
-git clone https://github.com/bheebsh/sublime-text-config.git ~/.config/sublime-text-3/Packages/User
-
-# Set python correctly
+# Set python symbolic link to python3
 ln -s /usr/bin/python3 /usr/bin/python
 ln -s /usr/bin/pip3 /usr/bin/pip
 
 sudo -H pip install -U radian 
+
+# Install Stata
+wget  -P ~/.restore/ "https://www.dropbox.com/s/r5s0pk3qduq1j67/Stata16Linux64.tar.gz?dl=0"
+sudo -H bash ~/.restore/stata-install.sh
