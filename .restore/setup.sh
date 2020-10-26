03b531a590f1030e794da254aa19729ec7e82480
Ben Heebsh <25202029+bheebsh@users.noreply.github.com>
2020-10-25 18:35:38 -0700

    Update setup.sh

--- .restore/setup.sh
+++ .restore/setup.sh
@@ -17,5 +17,7 @@ wget  -P ~/.restore/ "https://www.dropbox.com/s/r5s0pk3qduq1j67/Stata16Linux64.t
 sudo -H bash ~/.restore/stata-install.sh
 
 # Install Julia
+cd /opt
 wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.2-linux-x86_64.tar.gz
 tar xzvf julia-1.5.2-linux-x86_64.tar.gz
+ln -s /opt/julia-1.5.2/bin/julia /usr/local/bin/julia
