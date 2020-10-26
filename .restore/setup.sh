f7f4d5364af37c5a6c22f4de2ca5b0d96ad6591e
Ben Heebsh <25202029+bheebsh@users.noreply.github.com>
2020-10-25 18:24:32 -0700

    Update setup.sh

--- .restore/setup.sh
+++ .restore/setup.sh
@@ -3,21 +3,12 @@
 # Setup script to restore installation
 
 # install packages
-sudo apt update
-sudo apt upgrade
 xargs -a ~/.restore/packages sudo apt install -y 
-
-# Change default shell to fish
-chsh -s /bin/fish
 
 # git setup
 git config --global status.showUntrackedFiles no
 git config --global user.name "Benjamin Heebsh"
 git config --global user.email "benjamin.heebsh@gmail.com"
-
-# Set python symbolic link to python3
-ln -s /usr/bin/python3 /usr/bin/python
-ln -s /usr/bin/pip3 /usr/bin/pip
 
 sudo -H pip install -U radian 
 
